b571ea56d957702b91d7b4e5d48df5a7ee5cf165
slowfoxxx22 <slowfoxxx22@gmail.com>
2025-03-08 19:43:21 -0300

    asdsada

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 let innerCursor = document.querySelector('.inner-cursor')
 let outerCursor = document.querySelector('.outer-cursor')
+
 document.addEventListener('mousemove', moveCursor)
 
 function moveCursor(e) {
     let x = e.clientX;
     let y = e.clientY;
 
-    innerCursor.computedStyleMap.left = `${x}px`
-    innerCursor.computedStyleMap.top = `${y}px`
-    outerCursor.computedStyleMap.left = `${x}px`
-    outerCursor.computedStyleMap.top = `${y}px`
+    innerCursor.style.left = `${x}px`
+    innerCursor.style.top = `${y}px`
+    outerCursor.style.left = `${x}px`
+    outerCursor.style.top = `${y}px`
 }
